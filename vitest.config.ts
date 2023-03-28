cf49a55a92da2fd6e240c3297e36728430d40dca
Mathew Topper <damm_horse@yahoo.co.uk>
2023-03-28 11:56:36 +0100

    chore: add coverage reporting

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
+    coverage: {
+      reporter: ['text', 'lcov'],
+      all: true,
+      exclude: ['!tests/mock/lambda', 'tests/mock', 'tests', 'lambda', '*.config.*']
+    },
   },
 })
